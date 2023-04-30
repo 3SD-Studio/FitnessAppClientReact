dcf5ea44a23441fa39d2436271c825758a70afd5
Michał Pilch <m.pilch2002@gmail.com>
2023-04-30 17:28:33 +0200

    API TEST

--- src/index.js
+++ src/index.js
@@ -11,7 +11,13 @@ root.render(
   </React.StrictMode>
 );
 
+// TEST IF CODE CAN ACCESS API. ONLY FOR TESTING PURPOSES
+fetch("https://localhost:7093/WeatherForecast").then((response) => response.json()).then((data) => console.log(data));
+
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
