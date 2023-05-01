3942c699758db5b7aeed58b9a8dfb6c2c9bb8cd4
Dave <dj300758@student.polsl.pl>
2023-05-02 01:07:20 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>DZIAŁA</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1>DZIAŁA</h1>
+      </div>
+      <div>
+        <p>Siemano</p>
+      </div>
+    </>
   );
 }
 
